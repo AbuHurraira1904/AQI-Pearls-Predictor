ffad5f55e976bff4f0eaee3172057588f3758967
AbuHurraira1904 <abuhurraira1904@gmail.com>
2026-09-01 16:53:44 +0500

    Logger is now forced and implemented abstration for Hopsworks_client and feature store's logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
+import hopsworks_client
 from fetch import fetch_all_uids
-from store import get_feature_store, get_latest_row, save_to_feature_store
+from store import get_latest_row, save_to_feature_store
 from validate import validate_aqi_data
 from aggregate import assemble_aggregated_data
 
@@ -14,7 +15,8 @@ def setup_logging():
         handlers=[
             logging.FileHandler("FETCHER.log"),
             logging.StreamHandler()
-        ]
+        ],
+        force=True,
     )
 
 def main():
@@ -23,7 +25,7 @@ def main():
     aqi_data = fetch_all_uids()
     valid_aqi_data = validate_aqi_data(aqi_data)
 
-    fs = get_feature_store()
+    fs = hopsworks_client.get_feature_store()
     last_feature_group = get_latest_row(fs, FEATURE_GROUP_NAME, FEATURE_GROUP_VERSION)
 
     fg_row = assemble_aggregated_data(valid_aqi_data, last_feature_group)
