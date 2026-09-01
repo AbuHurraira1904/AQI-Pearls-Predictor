7ff086dd300779931df902f6978f6544c230a8ad
AbuHurraira1904 <abuhurraira1904@gmail.com>
2026-09-01 17:03:39 +0500

    Removed a useless import

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 import logging
  
 import numpy as np
-import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
