dff32b0b4f732e53ed5bacca04926d9bbc6a1145
AbuHurraira1904 <abuhurraira1904@gmail.com>
2026-09-05 02:50:14 +0500

    Updated the training pipeline to use Feast Feature store instead of hopsworks

--- main_train.py
+++ main_train.py
@@ -2,14 +2,16 @@ import logging
 import hopsworks_client
 import pandas as pd
 from clean_backfill import get_cleaned_feature_data
-#from clean_extra_backfill import get_extra_data
 from data_preparation import prepare_training_data
 from train import train_all_horizons
-from store import get_all_rows
+#from store import get_all_rows
+from feast_store import get_all_rows
 from promote import promote_all
 
-FEATURE_GROUP_NAME = "hourly_city_aqi"
-FEATURE_GROUP_VERSION = 3
+#FEATURE_GROUP_NAME = "hourly_city_aqi"
+#FEATURE_GROUP_VERSION = 3
+
+REPO_PATH = "aqi_feature_repo/feature_repo"
 
 TEMPORARY_JSON = "temporary_fetch_group_storage.json"
 
@@ -30,13 +32,15 @@ def main():
     logger = logging.getLogger(__name__)
 
     '''This code pulls the data from hopsworks only and stores it on a temporary json'''
-    fs = hopsworks_client.get_feature_store()
-    df = get_all_rows(fs, FEATURE_GROUP_NAME, FEATURE_GROUP_VERSION)
-    df.to_json(TEMPORARY_JSON, orient="records", indent=4)
+    #fs = hopsworks_client.get_feature_store()
+    #df = get_all_rows(fs, FEATURE_GROUP_NAME, FEATURE_GROUP_VERSION)
+    #df.to_json(TEMPORARY_JSON, orient="records", indent=4)
 
     '''This code temporarily reads the raw_data from temporary json which saves us the api calls'''
     # df = pd.read_json(TEMPORARY_JSON, convert_dates=False)  # temp
     # logger.info("Loaded %d raw rows from %s", len(df), TEMPORARY_JSON)  # temp
+
+    df = get_all_rows()
 
     '''This code only cleans data from hopsworks only and returns the cleaned data'''
     cleaned_df = get_cleaned_feature_data(df)
