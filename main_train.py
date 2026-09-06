bf909349f56ff88b2284a2c64cf1e15502e0998b
AbuHurraira1904 <abuhurraira1904@gmail.com>
2026-09-06 06:58:23 +0500

    Results are now saved a local file

--- main_train.py
+++ main_train.py
@@ -1,10 +1,9 @@
 import logging
+import joblib
 import hopsworks_client
-import pandas as pd
 from clean_backfill import get_cleaned_feature_data
 from data_preparation import prepare_training_data
 from train import train_all_horizons
-#from store import get_all_rows
 from feast_store import get_all_rows
 from promote import promote_all
 
@@ -28,7 +27,6 @@ def setup_logging():
 
 def main():
     setup_logging()
-
     logger = logging.getLogger(__name__)
 
     '''This code pulls the data from hopsworks only and stores it on a temporary json'''
@@ -51,9 +49,10 @@ def main():
     prepared_df = prepare_training_data(cleaned_df)
     training_results = train_all_horizons(prepared_df)
 
+    joblib.dump(training_results, "training_results.pkl")
+    logger.info("Saved training_results to training_results.pkl for downstream explainability (SHAP notebook)")
 
     mr = hopsworks_client.get_model_registry()
-
     outcomes = promote_all(mr, training_results)
 
 if __name__ == "__main__":
